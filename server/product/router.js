0803095895662220bad1389918fc22150e55672c
learnerdilip <diliptalks@gmail.com>
2020-03-04 19:17:37 +0100

    added endpoint for adding product data

--- server/product/router.js
+++ server/product/router.js
@@ -3,4 +3,13 @@ const Product = require("./model");
 
 const router = new Router();
 
+router.post("/product", async (request, response, next) => {
+  try {
+    const productCreate = Product.create(request.body);
+    response.send(productCreate);
+  } catch (error) {
+    next(console.error);
+  }
+});
+
 module.exports = router;
--- server/room/router.js
+++ server/room/router.js
@@ -3,4 +3,6 @@ const Room = require("./model");
 
 const router = new Router();
 
+// router.get()
+
 module.exports = router;
