06b7a0d7b83bc02595de157d0693bd2f71e77de3
learnerdilip <diliptalks@gmail.com>
2020-03-05 10:43:52 +0100

    corrected the router for product creation

--- server/product/router.js
+++ server/product/router.js
@@ -5,7 +5,19 @@ const router = new Router();
 
 router.post("/product", async (request, response, next) => {
   try {
-    const productCreate = Product.create(request.body);
+    console.log("--the PRODUCT form req--", request.body);
+    const productCreate = await Product.create({
+      document_name: request.body.documentName,
+      name_on_invoice: request.body.nameOnInvoice,
+      device_name: request.body.deviceName,
+      purchase_date: request.body.purchaseDate,
+      warranty_start_date: request.body.warrantyStartDate,
+      warranty_end_date: request.body.warrantyEndDate,
+      warranty_doc_image: request.body.warrantyDocument,
+      other_image: request.body.miscellaneousImage
+    });
+    // console.log("---Backend pdt created----", productCreate);
+
     response.send(productCreate);
   } catch (error) {
     next(console.error);
--- server/room/router.js
+++ server/room/router.js
@@ -3,6 +3,16 @@ const Room = require("./model");
 
 const router = new Router();
 
-// router.get()
+router.post("/room", async (request, response, next) => {
+  try {
+    console.log("---the request body of rrom post --", request.body);
+    const createdRoom = await Room.create({
+      room_name: request.body.roomName
+    });
+    response.send(createdRoom);
+  } catch {
+    error => next(error);
+  }
+});
 
 module.exports = router;
