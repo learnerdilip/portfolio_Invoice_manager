213c28da54dfb162a7c6fb0d7cf922080cf6af8b
learnerdilip <diliptalks@gmail.com>
2020-03-06 13:23:54 +0100

    added the signup mail sending

--- server/user/router.js
+++ server/user/router.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const User = require("../user/model");
 const bcrypt = require("bcrypt");
 const { toJWT } = require("../auth/jwt");
+const nodemailer = require("nodemailer");
 
 const router = new Router();
 
@@ -16,7 +17,33 @@ router.post("/signup", (request, response, next) => {
   const hashedPassword = bcrypt.hashSync(request.body.password, 10); //hash password using bcrypt
   const user = { ...request.body, password: hashedPassword };
   User.create(user)
-    .then(user => response.send(user))
+    .then(user => {
+      response.send(user);
+      //sending mails
+      var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "paladinistormentor@gmail.com",
+          pass: "paladin#1"
+        }
+      });
+
+      var mailOptions = {
+        from: "paladinistormentor@gmail.com",
+        to: user.email,
+        subject: "Thank you for signing up on Invoice Manager created by Dilip",
+        text:
+          "Thank you for signing up, you can now manage your warranty documents for your home appliances very easily"
+      };
+
+      transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
+    })
     .catch(e => {
       // console.log(JSON.stringify(e.errors[0].message, null, 2));
       response.send({
