26f2c28c7d589391d580deacfb1cd29a88ef9de2
learnerdilip <diliptalks@gmail.com>
2020-03-08 16:14:09 +0100

    removed logs

--- client/src/store/product/action.js
+++ client/src/store/product/action.js
@@ -3,9 +3,7 @@ import axios from "axios";
 const baseUrl = `http://localhost:4000`;
 
 export const createProduct = productFormData => async dispatch => {
-  // console.log("---the product form data sent in action-----", productFormData);
   const response = await axios.post(`${baseUrl}/product`, productFormData);
-  console.log("product creation response data---", response.data);
   dispatch(productCreated(response.data));
 };
 const productCreated = data => {
@@ -16,9 +14,7 @@ const productCreated = data => {
 };
 
 export const updateRoomProduct = roomId => async dispatch => {
-  // console.log("----the room getting action---", roomId);
   const response = await axios.get(`${baseUrl}/products?roomId=${roomId}`);
-  console.log("---response---", response);
   dispatch(productsFetched(response.data));
 };
 const productsFetched = data => {
