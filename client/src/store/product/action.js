b00409b636c018e246c7d992fa704ccd1dd70fba
learnerdilip <diliptalks@gmail.com>
2020-03-08 16:05:42 +0100

    added image setting for document page

--- client/src/store/product/action.js
+++ client/src/store/product/action.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const baseUrl = `http://localhost:4000`;
 
 export const createProduct = productFormData => async dispatch => {
-  console.log("---the product form data sent-----", productFormData);
+  // console.log("---the product form data sent in action-----", productFormData);
   const response = await axios.post(`${baseUrl}/product`, productFormData);
-  // console.log("product creation response data---", response.data);
+  console.log("product creation response data---", response.data);
   dispatch(productCreated(response.data));
 };
 const productCreated = data => {
@@ -16,7 +16,9 @@ const productCreated = data => {
 };
 
 export const updateRoomProduct = roomId => async dispatch => {
+  // console.log("----the room getting action---", roomId);
   const response = await axios.get(`${baseUrl}/products?roomId=${roomId}`);
+  console.log("---response---", response);
   dispatch(productsFetched(response.data));
 };
 const productsFetched = data => {
