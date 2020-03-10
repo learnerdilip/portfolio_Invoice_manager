eb616a3662b42586674b122c3f6ee8e7763c193a
learnerdilip <diliptalks@gmail.com>
2020-03-10 11:47:37 +0100

    added edit feature and added the mailing ilst model

--- server/product/router.js
+++ server/product/router.js
@@ -2,11 +2,12 @@ const { Router } = require("express");
 const Product = require("./model");
 const auth = require("../auth/middleWare");
 const cron = require("node-cron");
-var moment = require("moment");
+const moment = require("moment");
+const MailingList = require("../mailing/model");
 
 const router = new Router();
 
-router.post("/product", async (request, response, next) => {
+router.post("/product", auth, async (request, response, next) => {
   try {
     // console.log("--the PRODUCT form req--", request.body);
     const productCreated = await Product.create({
@@ -20,9 +21,16 @@ router.post("/product", async (request, response, next) => {
       other_image: request.body.miscellaneousImage,
       roomId: request.body.roomId
     });
-    //calculating number of days while creation
-    const item = findRemainingDays(productCreated);
-    response.send(item);
+    //adding to the mailnig list
+    const userEmail = request.user.dataValues.email; //find user email
+    const addToMailing = await MailingList.create({
+      email_id: userEmail,
+      product_id: productCreated.dataValues.id,
+      total_warranty: findTotalDays(productCreated),
+      remaining_warranty: findRemainingDays(productCreated)
+    });
+    // console.log("the addtomailining--------", addToMailing);
+    response.send(productCreated);
   } catch (error) {
     next(console.error);
   }
@@ -33,14 +41,24 @@ router.get("/products", async (request, response, next) => {
     const productsArray = await Product.findAll({
       where: { roomId: request.query.roomId }
     });
+    response.send(productsArray);
+  } catch (error) {
+    next(console.error);
+  }
+});
 
-    //calculating number of days remaining
-    const newArr = productsArray.map(prod => {
-      const item = findRemainingDays(prod);
-      return item;
-    });
-    //console.log("the pdt arr = ", newArr);
-    response.send(newArr);
+router.put("/product", async (request, response, next) => {
+  try {
+    // console.log("the request for edit", request.body);
+    const findProd = await Product.findByPk(request.body.id);
+    (findProd.document_name = request.body.documentName),
+      (findProd.name_on_invoice = request.body.nameOnInvoice),
+      (findProd.device_name = request.body.deviceName),
+      (findProd.purchase_date = request.body.purchaseDate),
+      (findProd.warranty_start_date = request.body.warrantyStartDate),
+      (findProd.warranty_end_date = request.body.warrantyEndDate),
+      findProd.save();
+    response.send(findProd);
   } catch (error) {
     next(console.error);
   }
@@ -62,12 +80,22 @@ router.delete("/product", async (request, response, next) => {
 // });
 
 const findRemainingDays = product => {
-  const item = product.dataValues;
+  const item = product.dataValues; //the item is a databse type object
   var een = moment(item.warranty_end_date);
   var twee = new Date();
   var duration = moment.duration(een.diff(twee));
-  item.daysRemaining = Math.floor(duration.as("days"));
-  return item; //a item with days remaining property
+  const daysRemaining = Math.floor(duration.as("days"));
+  // console.log("---the ramining days", daysRemaining);
+  return daysRemaining; //a item with days remaining property
+};
+
+const findTotalDays = product => {
+  const item = product.dataValues;
+  const een = moment(item.warranty_end_date);
+  const twee = moment(item.warranty_start_date);
+  const duration = moment.duration(een.diff(twee)).days();
+  // console.log("---the Total duration days", duration);
+  return duration;
 };
 
 module.exports = router;
